c1e3ef31d148c373078eb35cb0c6cc991746962c
Matt Moore <illogical@users.noreply.github.com>
2024-10-10 20:56:28 -0400

    Add project files.

---
+++ Controllers/ActivateController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RaspApi.Services;
+
+
+namespace RaspApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ActivateController : ControllerBase
+    {
+        private PiService _piService;
+        private readonly ILogger<ActivateController> _logger;
+
+        public ActivateController(ILogger<ActivateController> logger)
+        {
+            _piService = new PiService();
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+        [HttpGet(Name = "On")]
+        [Route("On")]
+        public IActionResult On()
+        {
+            try
+            {
+                _piService.EnableLED();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+
+                return StatusCode(500, new { ex.Message });
+            }
+
+            return Ok();
+        }
+
+        [HttpGet(Name = "Off")]
+        [Route("Off")]
+        public IActionResult Off()
+        {
+            try
+            {
+                _piService.DisableLED();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+
+                return StatusCode(500, new { ex.Message });
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ Services/PiService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Device.Gpio;
+
+namespace RaspApi.Services
+{
+    public class PiService
+    {
+        public bool LEDOn { get; set; }
+
+        private int _ledPin = 18;
+
+        public PiService()
+        {
+        }
+
+
+        //public void ToggleLED()
+        //{
+        //    using var controller = new GpioController();
+        //    controller.OpenPin(_ledPin, PinMode.Output);
+        //    controller.Write(_ledPin, ((LEDOn) ? PinValue.High : PinValue.Low));
+        //    LEDOn = !LEDOn;
+        //}
+
+        public void EnableLED()
+        {
+            using (var controller = new GpioController())
+            {
+                controller.OpenPin(_ledPin, PinMode.Output);
+                controller.Write(_ledPin, PinValue.High);
+            }
+        }
+
+        public void DisableLED()
+        {
+            using (var controller = new GpioController())
+            {
+                controller.OpenPin(_ledPin, PinMode.Output);
+                controller.Write(_ledPin, PinValue.Low);
+            }
+        }
+
+    }
+}
