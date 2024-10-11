b0491c709be1b4a84d91fef6a77d846db0441795
Matt Moore <illogical@users.noreply.github.com>
2024-10-10 20:59:35 -0400

    Minor cleanup

--- Services/PiService.cs
+++ Services/PiService.cs
@@ -5,22 +5,7 @@ namespace RaspApi.Services
 {
     public class PiService
     {
-        public bool LEDOn { get; set; }
-
         private int _ledPin = 18;
-
-        public PiService()
-        {
-        }
-
-
-        //public void ToggleLED()
-        //{
-        //    using var controller = new GpioController();
-        //    controller.OpenPin(_ledPin, PinMode.Output);
-        //    controller.Write(_ledPin, ((LEDOn) ? PinValue.High : PinValue.Low));
-        //    LEDOn = !LEDOn;
-        //}
 
         public void EnableLED()
         {
